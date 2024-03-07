fd9f509325d093f08eb18db6c3a7955a0af106c6
madisec <madiisec@gmail.com>
2024-03-07 19:16:33 +0330

    Understanding how blockchain works

---
+++ index.js
@@ -0,0 +1,35 @@
+const hashFunc = (input) => {
+    return '*' + input + '*';
+}
+
+class Block {
+    constructor(data, hash, lastHash) {
+        this.data = data;
+        this.hash = hash;
+        this.lastHash = lastHash;
+    }
+}
+
+class BlockChain {
+    constructor() {
+        const genesis = new Block('Gen-Data', 'Gen-Hash', 'Gen-LastHash');
+        this.chain = [genesis];
+
+    }
+
+    addBlock(data) {
+        const lastHash  = this.chain[this.chain.length - 1].hash;
+        const hash = hashFunc(data + lastHash);
+        const block  = new Block(data, hash, lastHash);
+        this.chain.push(block);
+
+    }
+}
+
+const fooBlockchain = new BlockChain();
+fooBlockchain.addBlock('One');
+fooBlockchain.addBlock('Tow');
+fooBlockchain.addBlock('three');
+
+console.log(fooBlockchain);
+
