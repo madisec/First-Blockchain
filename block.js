ea2140c4b78e237f1c325ce8a54f1be6edb8bdf6
madisec <madiisec@gmail.com>
2024-03-09 23:35:03 +0330

    remove index.js

--- block.js
+++ block.js
@@ -1,16 +1,18 @@
 class Block {
-  constructor({ timeStamp, lastHash, hash, data }) {
-    this.timeStamp = timeStamp;
-    this.lastHash = lastHash;
-    this.hash = hash;
-    this.data = data;
-  }
+//   constructor({ timeStamp, lastHash, hash, data }) {
+//     this.timeStamp = timeStamp;
+//     this.lastHash = lastHash;
+//     this.hash = hash;
+//     this.data = data;
+//   }
 }
 
-const block1 = new Block({
-  timeStamp: "14344345354",
-  lastHash: "bar-lastHash",
-  hash: "hash",
-  data: "bar-data",
-});
-console.log(block1);
+// const block1 = new Block({
+//   timeStamp: "14344345354",
+//   lastHash: "bar-lastHash",
+//   hash: "hash",
+//   data: "bar-data",
+// });
+// console.log(block1);
+
+module.exports = Block;
